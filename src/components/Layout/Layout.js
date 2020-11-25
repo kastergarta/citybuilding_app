e796ebda3dd4a41da722cdb27862c1e6c17ff854
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-11-24 22:42:10 -0500

    Navigation 2 SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-//import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
