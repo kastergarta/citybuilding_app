f84e67dc4bb0541d8a5b3b024bd7a7bac401aef7
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-11-29 21:03:22 -0500

    new file structure + axios + firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
@@ -16,4 +16,3 @@ class App extends Component {
 }
 
 export default App;
- 
